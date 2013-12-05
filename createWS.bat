0b9d44df6bcf7de9cc8f23e51288cf18d9497685
Urban Hedeås <urban.hedeas@gmail.com>
2013-12-05 16:07:51 +0100

    Init

---
+++ createWS.bat
@@ -0,0 +1,1 @@
+mvn archetype:generate  -DarchetypeGroupId=org.apache.maven.archetypes  -DgroupId=se.sweco.3dvis -DartifactId=portal  -DinteractiveMode=false
---
+++ portal/src/main/java/se/sweco/3dvis/App.java
@@ -0,0 +1,13 @@
+package se.sweco.3dvis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
